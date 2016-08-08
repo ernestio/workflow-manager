bb4a24edfbc83b6a51f5e04d0d737bfffb8a40fa
Adria Cidre <adria.cidre@gmail.com>
2016-08-08 17:33:32 +0200

    Ernest-config-client based service

--- config.go
+++ config.go
@@ -4,18 +4,8 @@
 
 package main
 
-import (
-	"log"
-	"os"
-
-	"github.com/nats-io/nats"
-)
-
 // Config : struct representation of service configuration
 type Config struct {
-	Nats struct {
-		URL string `json:"url"`
-	} `json:"nats"`
 	SaltAuthentication struct {
 		User     string `json:"user"`
 		Password string `json:"password"`
@@ -24,18 +14,6 @@ type Config struct {
 
 // Load : will load configuration from the given path
 func (c *Config) Load() {
-	c.Nats.URL = os.Getenv("NATS_URI")
 	c.SaltAuthentication.User = ""
 	c.SaltAuthentication.Password = ""
-}
-
-// NatsClient : creates a new nats client
-func (c *Config) NatsClient() *nats.Conn {
-	n, err := nats.Connect(c.Nats.URL)
-	if err != nil {
-		log.Println("Could not connect to NATS server")
-		panic(err)
-	}
-
-	return n
 }
