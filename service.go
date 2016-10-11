fb9986f2fd6cc63184659446ee419b49f91b9ba2
Tom Bevan <tom.bevan@r3labs.io>
2016-10-11 16:33:13 +0100

    ernestio/ernest#6: mapping elb components

--- service.go
+++ service.go
@@ -244,6 +244,37 @@ type router struct {
 	status
 }
 
+// ELBPort ...
+type elbPort struct {
+	FromPort int    `json:"from_port"`
+	ToPort   int    `json:"to_port"`
+	Protocol string `json:"protocol"`
+	SSLCert  string `json:"ssl_cert"`
+}
+
+// ELB : Mapping for a elb component
+type elb struct {
+	Type                string    `json:"elb_type"`
+	Name                string    `json:"elb_name"`
+	IsPrivate           bool      `json:"elb_is_private"`
+	DNSName             string    `json:"elb_dns_name"`
+	Ports               []elbPort `json:"elb_ports"`
+	NetworkAWSIDs       []string  `json:"network_aws_ids"`
+	Instances           []string  `json:"instances"`
+	InstanceAWSIDs      []string  `json:"instance_aws_ids"`
+	SecurityGroups      []string  `json:"security_groups"`
+	SecurityGroupAWSIDs []string  `json:"security_group_aws_ids"`
+	DatacenterType      string    `json:"datacenter_type,omitempty"`
+	DatacenterName      string    `json:"datacenter_name,omitempty"`
+	DatacenterRegion    string    `json:"datacenter_region"`
+	DatacenterToken     string    `json:"datacenter_token"`
+	DatacenterSecret    string    `json:"datacenter_secret"`
+	VpcID               string    `json:"vpc_id"`
+	Service             string    `json:"service"`
+	Status              string    `json:"status"`
+	Exists              bool
+}
+
 // This is the object representation for a service inside the
 // FSM, it has appended the workflow the service needs to
 // follow to be built
@@ -455,6 +486,34 @@ type service struct {
 		status
 		SequentialProcessing bool `json:"sequential_processing"`
 	} `json:"routers_to_delete"`
+	ELBs struct {
+		Started              string `json:"started"`
+		Finished             string `json:"finished"`
+		Status               string `json:"status"`
+		Items                []elb  `json:"items"`
+		SequentialProcessing bool   `json:"sequential_processing"`
+	} `json:"elbs"`
+	ELBsToCreate struct {
+		Started              string `json:"started"`
+		Finished             string `json:"finished"`
+		Status               string `json:"status"`
+		Items                []elb  `json:"items"`
+		SequentialProcessing bool   `json:"sequential_processing"`
+	} `json:"elbs_to_create"`
+	ELBsToUpdate struct {
+		Started              string `json:"started"`
+		Finished             string `json:"finished"`
+		Status               string `json:"status"`
+		Items                []elb  `json:"items"`
+		SequentialProcessing bool   `json:"sequential_processing"`
+	} `json:"elbs_to_update"`
+	ELBsToDelete struct {
+		Started              string `json:"started"`
+		Finished             string `json:"finished"`
+		Status               string `json:"status"`
+		Items                []elb  `json:"items"`
+		SequentialProcessing bool   `json:"sequential_processing"`
+	} `json:"elbs_to_delete"`
 }
 
 type message struct {
