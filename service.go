8a2d8a0cde3962ccffdfb36ba14caf847ecca2f4
Tom Bevan <tom.bevan@r3labs.io>
2016-10-12 12:18:47 +0100

    standardising fields

--- service.go
+++ service.go
@@ -244,8 +244,8 @@ type router struct {
 	status
 }
 
-// ELBPort ...
-type elbPort struct {
+// elbListener ...
+type elbListener struct {
 	FromPort int    `json:"from_port"`
 	ToPort   int    `json:"to_port"`
 	Protocol string `json:"protocol"`
@@ -254,24 +254,24 @@ type elbPort struct {
 
 // ELB : Mapping for a elb component
 type elb struct {
-	Type                string    `json:"_type"`
-	Name                string    `json:"elb_name"`
-	IsPrivate           bool      `json:"elb_is_private"`
-	DNSName             string    `json:"elb_dns_name"`
-	Ports               []elbPort `json:"elb_ports"`
-	NetworkAWSIDs       []string  `json:"network_aws_ids"`
-	Instances           []string  `json:"instances"`
-	InstanceAWSIDs      []string  `json:"instance_aws_ids"`
-	SecurityGroups      []string  `json:"security_groups"`
-	SecurityGroupAWSIDs []string  `json:"security_group_aws_ids"`
-	DatacenterType      string    `json:"datacenter_type,omitempty"`
-	DatacenterName      string    `json:"datacenter_name,omitempty"`
-	DatacenterRegion    string    `json:"datacenter_region"`
-	DatacenterToken     string    `json:"datacenter_token"`
-	DatacenterSecret    string    `json:"datacenter_secret"`
-	VpcID               string    `json:"vpc_id"`
-	Service             string    `json:"service"`
-	Status              string    `json:"status"`
+	Type                string        `json:"_type"`
+	Name                string        `json:"name"`
+	IsPrivate           bool          `json:"is_private"`
+	DNSName             string        `json:"dns_name"`
+	Listeners           []elbListener `json:"listeners"`
+	NetworkAWSIDs       []string      `json:"network_aws_ids"`
+	Instances           []string      `json:"instances"`
+	InstanceAWSIDs      []string      `json:"instance_aws_ids"`
+	SecurityGroups      []string      `json:"security_groups"`
+	SecurityGroupAWSIDs []string      `json:"security_group_aws_ids"`
+	DatacenterType      string        `json:"datacenter_type,omitempty"`
+	DatacenterName      string        `json:"datacenter_name,omitempty"`
+	DatacenterRegion    string        `json:"datacenter_region"`
+	DatacenterToken     string        `json:"datacenter_token"`
+	DatacenterSecret    string        `json:"datacenter_secret"`
+	VpcID               string        `json:"vpc_id"`
+	Service             string        `json:"service"`
+	Status              string        `json:"status"`
 	Exists              bool
 }
 
